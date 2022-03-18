b549054ded4e5503b80c9cd92d2f6ee445db0bfd
Maria Abritta <87709987+MariaAbritta@users.noreply.github.com>
2022-03-18 09:08:01 -0300

    arrumando sintaxe

--- TP03.git/TestTP3/src/models/Bicicleta.java
+++ TP03.git/TestTP3/src/models/Bicicleta.java
@@ -80,7 +80,7 @@ public class Bicicleta {
 					switch(opAluguelUrbana) {
 						case "sim":
 							System.out.println("Qual localizacao voce se encontra? ");
-							//System.out.println(Bicicletario.getLocalizacao()); Puxar as localizações que temos bicicletarios e entregar o id da bike que ele quer
+							//System.out.println(Bicicletario.getLocalizacao()); Puxar as localizaÃ§Ãµes que temos bicicletarios e entregar o id da bike que ele quer
 							//Criar um array das bikes Urbanas
 							//Repetir com os outros 2 tipos de bike
 						break;
@@ -157,10 +157,7 @@ public class Bicicleta {
 
 /*
  * Usar os gets e sets
-<<<<<<< HEAD
- * Fazer os arryas de cada tipo de bike e quantas v�o ter 
-=======
  * Fazer os arryas de cada tipo de bike e quantas vão ter 
->>>>>>> branch 'main' of https://github.com/MariaAbritta/TP03.git
+ * Fazer os arryas de cada tipo de bike e quantas vÃ£o ter 
  * Fazer a ponte com o bicicletario
  * */
