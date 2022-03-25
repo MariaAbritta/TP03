d0ec2a3ec813c9521273ff5d1d4620170d7d11ca
MariaAbritta <eduardaabritta@gmail.com>
2022-03-25 12:00:45 -0300

    fazendo listagem

--- TP03.git/TestTP3/src/models/Pagamento.java
+++ TP03.git/TestTP3/src/models/Pagamento.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 public class Pagamento {
 	private String formaDePagamento;
 	private double valorTotalPagamento;
-	private float finalTotalViagem;
-	private double numeroDoCartao;
+	private String numeroDoCartao;
 	private String nomeNoCartao;
-	private int codigoDeSeguranca;
+	private String codigoDeSeguranca;
+	static  int i = 0, j = 1;
 	static Scanner ler = new Scanner(System.in);
 	
 	public Pagamento() {
 		super();
 	}
 	
-	public Pagamento(String fPagamento, double vTotal, float fTempo, double numCartao, String nomeCartao, int cvc) {
+	public Pagamento(String fPagamento, double vTotal, String numCartao, String nomeCartao, String cvc) {
 		this.formaDePagamento = fPagamento;
 		this.valorTotalPagamento = vTotal;
-		this.finalTotalViagem = fTempo;
 		this.numeroDoCartao = numCartao;
 		this.nomeNoCartao = nomeCartao;
 		this.codigoDeSeguranca = cvc;
@@ -44,22 +43,11 @@ public class Pagamento {
 		this.valorTotalPagamento = valorTotalPagamento;
 	}
 
-	public float getFinalTotalViagem() {
-		
-		return finalTotalViagem;
-	}
-
-	public void setFinalTotalViagem(float finalTotalViagem) {
-		//Tempo tempo = new Tempo();
-		//finalTotalViagem = tempo.getMinutosViagem();
-		this.finalTotalViagem = finalTotalViagem;
-	}
-
-	public double getNumeroDoCartao() {
+	public String getNumeroDoCartao() {
 		return numeroDoCartao;
 	}
 
-	public void setNumeroDoCartao(double numeroDoCartao) {
+	public void setNumeroDoCartao(String numeroDoCartao) {
 		this.numeroDoCartao = numeroDoCartao;
 	}
 
@@ -71,35 +59,117 @@ public class Pagamento {
 		this.nomeNoCartao = nomeNoCartao;
 	}
 
-	public int getCodigoDeSeguranca() {
+	public String getCodigoDeSeguranca() {
 		return codigoDeSeguranca;
 	}
 
-	public void setCodigoDeSeguranca(int codigoDeSeguranca) {
+	public void setCodigoDeSeguranca(String codigoDeSeguranca) {
 		this.codigoDeSeguranca = codigoDeSeguranca;
 	}
 	
 	//metodos
 	public void cadastrarPagamento() {
-		System.out.println("--------------------");
-		System.out.println("\nCadastrar Pagamento: \n");
-		
+		System.out.println("------------------------------");
+		System.out.println("Pagamento: ");
 		System.out.println("Forma de Pagamento(debito, credito):\n");
+		// Validacao do Pagamento:
 		setFormaDePagamento(ler.next());
-		
-		System.out.println("Valor a ser pago: " + getValorTotalPagamento());
-		System.out.println("Tempo total da viagem: " + getFinalTotalViagem());
+		boolean validarCartao = true;
+		String respostaCartao;
+		respostaCartao = getFormaDePagamento();
+		if ((respostaCartao.contains("debito")) || (respostaCartao.contains("credito"))){ 
+			validarCartao = false;
+		}else{  
+			do {
+				System.out.println("Erro! Escolha apenas debito ou credito: \n");
+				setFormaDePagamento(ler.next());
+				respostaCartao = getFormaDePagamento();
+				if ((respostaCartao.contains("debito")) || (respostaCartao.contains("credito"))){ 
+					validarCartao = false;
+				}
+			}while(validarCartao == false);
+		}
+		/////////////////////////////////////////////////////////////
+		System.out.println("Valor a ser pago: " + getValorTotalPagamento()); //Como linkar com o pedido da pessoa?
 		
 		System.out.println("Cadastrar numero do cartao para pagamento: \n");
-		setNumeroDoCartao(ler.nextDouble());
-		
-		System.out.println("Cadastrar nome no cartao: \n");
+		// Validacao do numero de pagamento
+		setNumeroDoCartao(ler.next());
+		boolean validarNumCartao = true;
+		String respostaNumCartao;
+		respostaNumCartao = getNumeroDoCartao();
+		if(respostaNumCartao.length() != 16){
+			validarNumCartao = false;
+			do {
+				System.out.println("Erro! Um cartao possui 16 digitos de identificacao. Insira os numeros validos:");
+				setNumeroDoCartao(ler.next());
+				respostaNumCartao = getNumeroDoCartao();
+				if(respostaNumCartao.length() != 16){
+					validarNumCartao = false;
+				}
+			}while(validarNumCartao == false);
+		}
+		if(!respostaNumCartao.substring(0).matches("[0-9]*")){
+			validarNumCartao = false;
+			do {
+				System.out.println("Erro! Digitos de identificacao possui apenas numeros. Insira os numeros validos:");
+				setNumeroDoCartao(ler.next());
+				respostaNumCartao = getNumeroDoCartao();
+				if(!respostaNumCartao.substring(0).matches("[0-9]*")){
+					validarNumCartao = false;
+				}
+			}while(validarNumCartao == false);
+		}
+		/////////////////////////////////////////////////////////////
+		System.out.println("Cadastrar nome do cartao: \n");
+		// Validacao do NOME de apenas letras:
 		setNomeNoCartao(ler.next());
-		
+		boolean validarNome = true;
+		String respostaNome;
+		respostaNome = getNomeNoCartao();
+		if(!respostaNome.substring(0).matches("[A-Z]*")){
+			validarNome = false;
+			do {
+				System.out.println("Erro! Um nome precisa ter letras maiusculas. \n"
+						+ "Insira um nome valido: ");
+				setNomeNoCartao(ler.next());
+				respostaNome = getNomeNoCartao();
+				if(!respostaNome.substring(0).matches("[A-Z]*")){
+					validarNome = false;
+				}
+			}while(validarNome == false);
+		}
+		/////////////////////////////////////////////////////////////
 		System.out.println("Cadastrar codigo de seguranca do cartao: \n");
-		setCodigoDeSeguranca(ler.nextInt());
-		System.out.println("--------------------");
-		
+		// Validacao do CVV:
+		setCodigoDeSeguranca(ler.next());
+		boolean validarCVV = true;
+		String respostaCVV;
+		respostaCVV = getCodigoDeSeguranca();
+		if(respostaCVV.length() != 3){
+			validarCVV = false;
+			do {
+				System.out.println("Erro! Um CVV possui 3 digitos. Insira um CVV valido:");
+				setCodigoDeSeguranca(ler.next());
+				respostaCVV = getCodigoDeSeguranca();
+				if(respostaCVV.length() != 3){
+					validarCVV = false;
+				}
+			}while(validarCVV == false);
+		}
+		if(!respostaCVV.substring(0).matches("[0-9]*")){
+			validarCVV = false;
+			do {
+				System.out.println("Erro! Um CVV possui apenas numeros. Insira um CVV valido:");
+				setCodigoDeSeguranca(ler.next());
+				respostaCVV = getCodigoDeSeguranca();
+				if(!respostaCVV.substring(0).matches("[0-9]*")){
+					validarCVV = false;
+				}
+			}while(validarCVV == false);
+		}
+		/////////////////////////////////////////////////////////////
+		System.out.println("------------------------------");
 	}
 	
 	public void editarPagamento() {
@@ -108,20 +178,19 @@ public class Pagamento {
 		
 		//nao editaveis
 		System.out.println("Valor a ser pago: " + getValorTotalPagamento());
-		System.out.println("Tempo total da viagem: " + getFinalTotalViagem());
 		
 		//editaveis
 		System.out.println("Forma de Pagamento (debito, credito):\n");
 		setFormaDePagamento(ler.next());
 		
 		System.out.println("Cadastrar numero do cartao para pagamento: \n");
-		setNumeroDoCartao(ler.nextDouble());
+		setNumeroDoCartao(ler.next());
 		
 		System.out.println("Cadastrar nome no cartao: \n");
 		setNomeNoCartao(ler.next());
 		
 		System.out.println("Cadastrar codigo de seguranca do cartao: \n");
-		setCodigoDeSeguranca(ler.nextInt());
+		setCodigoDeSeguranca(ler.next());
 		System.out.println("--------------------");
 	}
 	
@@ -129,21 +198,22 @@ public class Pagamento {
 		System.out.println("\nDeletando Pagamento....\n");
 		setValorTotalPagamento(0);
 		setFormaDePagamento("");
-		setNumeroDoCartao(0);
+		setNumeroDoCartao("");
 		setNomeNoCartao("");
-		setCodigoDeSeguranca(0);
-		setFinalTotalViagem(0);
+		setCodigoDeSeguranca("");
 	}
 	
 	public void listarPagamento() {
-		System.out.println("\nListar Pagamento: \n");
-		System.out.println("\nValor a ser pago: " + getValorTotalPagamento());
-		System.out.println("\nTempo de utilizacao: " + getFinalTotalViagem());
-		System.out.println("\nForma de pagamento: " + getFormaDePagamento());
-		System.out.println("\nNumero do cartao: " + getNumeroDoCartao());
-		System.out.println("\nNome no cartao: " + getNomeNoCartao());
-		System.out.println("\nCodigo de seguranca: " + getCodigoDeSeguranca());
-		
+		System.out.println("------------------------------");
+		System.out.println("Listar Pagamento:");
+		System.out.println("Valor a ser pago: " + getValorTotalPagamento());
+		System.out.println("Forma de pagamento: " + getFormaDePagamento());
+		System.out.println("Numero do cartao: " + getNumeroDoCartao());
+		System.out.println("Nome no cartao: " + getNomeNoCartao());
+		System.out.println("Codigo de seguranca: " + getCodigoDeSeguranca());
+		System.out.println("PAGAMENTO SENDO PROCESSADO...");
+		System.out.println("...");
+		System.out.println("PAGAMENTO REALIZADO!");
 	}
 	
 }
