841bde0aeefaf33b6c5d7aa0651ffc27c62ca29f
MariaAbritta <eduardaabritta@gmail.com>
2022-04-01 01:31:52 -0300

    ajustando a tela inicial e criando as verificações

--- TP03.git/TestTP3/src/controler/ControlerUsuarios.java
+++ TP03.git/TestTP3/src/controler/ControlerUsuarios.java
@@ -1,333 +1,220 @@
 package controler;
+
 import java.util.Scanner;
 
 import models.*;
 
 public class ControlerUsuarios {
 
-		private Usuario user = new Usuario();
-		public static int qtdUser=0;
-		static 	int i = 0, j=0;
-		static Scanner ler = new Scanner(System.in);
-		
+	private Usuario user = new Usuario();
+	public static int qtdUser = 0;
+	static int i = 0, j = 0;
+	static Scanner ler = new Scanner(System.in);
 
-		public void CadastroUser() {
-			cadastroUsuario();
+	public void CadastroUser() {
+		cadastroUsuario();
+	}
+
+	public void editarUser() {
+		editarUsuario();
+	}
+
+	public void listarUsuario() {
+		for (int x = 0; x <= 6; x++) {
+			if (Usuario.usuario[ControlerLoging.indiceUser][x] != null) {
+				System.out.println(x + ". " + Usuario.usuario[ControlerLoging.indiceUser][x]);
+			}
 		}
-		public void editarUser() {
-			editarUsuario();
-		}
-		
-		public void listarUsuario() {
-			for(int x = 0; x <= 6 ; x++) {
-				if(Usuario.usuario[ControlerLoging.indiceUser][x] != null) {
-					System.out.println(x + ". " + Usuario.usuario[ControlerLoging.indiceUser][x]);
+	}
+
+	public void deletarUsuario() {
+		System.out.println("Deseja excluir usuario??");
+		System.out.println("1.SIM \n 2.NAO");
+		int confirmar = 0;
+		confirmar = ler.nextInt();
+
+		switch (confirmar) {
+			case 1:
+				for (int x = 0; x <= 6; x++) {
+					if (Usuario.usuario[ControlerLoging.indiceUser][x] != null) {
+						Usuario.usuario[ControlerLoging.indiceUser][x] = null;
+						System.out.println(x + ". " + Usuario.usuario[ControlerLoging.indiceUser][x]);
+					}
 				}
-			}
+				System.out.println("Deletado...	 ");
+				break;
+			case 2:
+				System.out.println("OK");
+				break;
+			default:
+				System.out.println("Nao tem essa opï¿½ï¿½o");
+				break;
 		}
-		
-		public void deletarUsuario() {
-			System.out.println("Deseja excluir usuario??");
-			System.out.println("1.SIM \n 2.NAO");
-			int confirmar =0;
-			confirmar = ler.nextInt();
-			
-			switch(confirmar){
-				case 1:
-					for(int x = 0; x <= 6 ; x++) {
-						if(Usuario.usuario[ControlerLoging.indiceUser][x] != null) {
-							Usuario.usuario[ControlerLoging.indiceUser][x] = null;
-							System.out.println(x + ". " + Usuario.usuario[ControlerLoging.indiceUser][x]);
-						}
-					}
-					System.out.println("Deletado...	 ");
-					break;
-				case 2:
-					System.out.println("OK");
-					break;
-				default:
-					System.out.println("Nao tem essa opção");
-					break;
-			}
-			
+
+	}
+
+	public void cadastroUsuario() {
+		// cadastrar nome
+		cadastrarNome();
+		Usuario.usuario[qtdUser][i] = user.getNome();
+		i++;
+		// cadastrar email
+		cadastrarEmail();
+		Usuario.usuario[qtdUser][i] = user.getEmail();
+		i++;
+		// cadastrar rg
+		cadastrarRg();
+		Usuario.usuario[qtdUser][i] = user.getRg();
+		i++;
+		// cadastrar cpf
+		cadastrarCpf();
+		Usuario.usuario[qtdUser][i] = user.getCpf();
+		i++;
+		// cadastrar senha
+		cadastrarSenha();
+		Usuario.usuario[qtdUser][i] = user.getSenha();
+		i++;
+		// cadastrar telefone
+		cadastrarTelefone();
+		Usuario.usuario[qtdUser][i] = user.getTelefone();
+		qtdUser++;
+		i = 0;
+	}
+
+	public void cadastrarNome() {
+		System.out.println("------------------------------");
+		System.out.println("Cadastrar usuarios: ");
+		System.out.println("Nome do usuario. Apenas letras maiusculas: ");
+		user.setNome(ler.next());
+		// validarNome();
+	}
+
+	public boolean validarNome(String nomeInput) {
+		System.out.println(nomeInput);
+		boolean verificar = true;
+		System.out.println("Validando Nome");
+		if (!nomeInput.substring(0).matches("[A-Z]*")) {
+			verificar = false;
 		}
-		
-		public void cadastroUsuario() {
-			//cadastrar nome
-			cadastrarNome();
-			Usuario.usuario[qtdUser][i] = user.getNome();
-			i++;
-			//cadastrar email
-			cadastrarEmail();
-			Usuario.usuario[qtdUser][i] = user.getEmail();
-			i++;
-			//cadastrar rg
-			cadastrarRg();
-			Usuario.usuario[qtdUser][i] = user.getRg();
-			i++;
-			//cadastrar cpf
-			cadastrarCpf();
-			Usuario.usuario[qtdUser][i] = user.getCpf();
-			i++;
-			//cadastrar senha
-			cadastrarSenha();
-			Usuario.usuario[qtdUser][i] = user.getSenha();
-			i++;
-			//cadastrar telefone
-			cadastrarTelefone();
-			Usuario.usuario[qtdUser][i] = user.getTelefone();
-			qtdUser++;
-			i=0;
+		return verificar;
+	}
+
+	public void cadastrarEmail() {
+		System.out.println("------------------------------");
+		System.out.println("Cadastrar usuarios: ");
+		System.out.println("Email do usuario. Apenas letras maiusculas: ");
+		user.setEmail(ler.next());
+		// validarEmail();
+	}
+
+	public boolean validarEmail(String emailValid) {
+		System.out.println("Validando Email");
+		boolean verificar = true;
+
+		if (!emailValid.substring(0).matches("(.*)@(.*)")) {
+			verificar = false;
 		}
-		
-		public void cadastrarNome() {
-			System.out.println("------------------------------");
-			System.out.println("Cadastrar usuarios: ");
-			System.out.println("Nome do usuario. Apenas letras maiusculas: ");
-			user.setNome(ler.next());
-			validarNome();
+		return verificar;
+	}
+
+	public void cadastrarRg() {
+		System.out.println("------------------------------");
+		System.out.println("Cadastrar usuarios: ");
+		System.out.println("Rg do usuario. Apenas letras maiusculas: ");
+		user.setRg(ler.next());
+		// validarRg();
+	}
+
+	public boolean validarRg(String rgValid) {
+		System.out.println(rgValid.length());
+		System.out.println("Validando RG");
+		boolean verificar = true;
+		if (rgValid.length() != 7) {
+			verificar = false;
 		}
-		
-		public void validarNome() {
-			boolean verificar = true;
-			System.out.println("Validando Nome");
-			String nomeValid = user.getNome();
-			if(!nomeValid.substring(0).matches("[A-Z]*")) {
-				verificar=false;
-				do {
-					System.out.println("Erro! Um nome precisa ter letras maiusculas. \n"
-							+ "Insira um nome valido: ");
-					user.setNome(ler.next());
-					nomeValid = user.getNome();
-					if(!nomeValid.substring(0).matches("[A-Z]*")){
-						verificar = false;
-					}else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
+		if (!rgValid.substring(0).matches("[0-9]*")) {
+			verificar = false;
 		}
-		
-		public void cadastrarEmail() {
-			System.out.println("------------------------------");
-			System.out.println("Cadastrar usuarios: ");
-			System.out.println("Email do usuario. Apenas letras maiusculas: ");
-			user.setEmail(ler.next());
-			validarEmail();
+
+		return verificar;
+	}
+
+	public void cadastrarCpf() {
+		System.out.println("------------------------------");
+		System.out.println("Cadastrar usuarios: ");
+		System.out.println("CPF do usuario. Apenas letras maiusculas: ");
+		user.setCpf(ler.next());
+		// validarCpf();
+	}
+
+	public boolean validarCpf(String cpfValid) {
+		System.out.println("Validando CPF");
+		boolean verificar = true;
+		if (cpfValid.length() != 11) {
+			verificar = false;
 		}
-		public void validarEmail() {
-			System.out.println("Validando Email");
-			boolean verificar = true;
-			String emailValid = user.getEmail();
-			if(!emailValid.substring(0).matches("(.*)@(.*)")){
-				verificar = false;
-				do {
-					System.out.println("Erro! Um e-mail precisa ter @. \n"
-							+ "Insira um e-mail valido: ");
-					user.setEmail(ler.next());
-					emailValid = user.getEmail();
-					if(!emailValid.substring(0).matches("(.*)@(.*)")){
-						verificar = false;
-					}
-					else {
-						verificar=true;
-					}
-				}while(verificar == false);
-			}
+		if (!cpfValid.substring(0).matches("[0-9]*")) {
+			verificar = false;
 		}
-		
-		public void cadastrarRg() {
-			System.out.println("------------------------------");
-			System.out.println("Cadastrar usuarios: ");
-			System.out.println("Rg do usuario. Apenas letras maiusculas: ");
-			user.setRg(ler.next());
-			validarRg();
+		return verificar;
+	}
+
+	public void cadastrarSenha() {
+		System.out.println("------------------------------");
+		System.out.println("Cadastrar usuarios: ");
+		System.out.println("Senha do usuario. Apenas letras maiusculas: ");
+		user.setSenha(ler.next());
+		// validarSenha();
+	}
+
+	public boolean validarSenha(String senhaValid) {
+		System.out.println(senhaValid);
+		System.out.println("Validando Senha");
+		boolean verificar = true;
+		if (senhaValid.length() != 7) {
+			verificar = false;
 		}
-		public void validarRg() {
-			System.out.println("Validando RG");
-			boolean verificar = true;
-			String rgValid = user.getRg();
-			if(rgValid.length() != 7){
-				verificar = false;
-				do {
-					System.out.println("Erro! Um RG possui 7 numeros. Insira um RG valido:");
-					user.setRg(ler.next());
-					rgValid = user.getRg();
-					if(rgValid.length() != 7){
-						verificar = false;
-					}
-					else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
-			if(!rgValid.substring(0).matches("[0-9]*")){
-				verificar = false;
-				do {
-					System.out.println("Erro! Um RG possui apenas numeros. Insira um RG valido:");
-					user.setRg(ler.next());
-					rgValid = user.getRg();
-					if(!rgValid.substring(0).matches("[0-9]*")){
-						verificar = false;
-					}
-				}while(verificar == false);
-			}
+		if (!senhaValid.substring(0, 3).matches("[A-Z]*")) {
+			verificar = false;
 		}
-		
-		public void cadastrarCpf() {
-			System.out.println("------------------------------");
-			System.out.println("Cadastrar usuarios: ");
-			System.out.println("CPF do usuario. Apenas letras maiusculas: ");
-			user.setCpf(ler.next());
-			validarCpf();
+		if (!senhaValid.substring(3).matches("[0-9]*")) {
+			verificar = false;
 		}
-		public void validarCpf() {
-			System.out.println("Validando CPF");
-			boolean verificar= true;
-			String cpfValid = user.getCpf();
-			if(cpfValid.length() != 11){
-				verificar = false;
-				do {
-					System.out.println("Erro! Um CPF possui 11 numeros. Insira um CPF valido: ");
-					user.setCpf(ler.next());
-					cpfValid = user.getCpf();
-					if(cpfValid.length() != 11){
-						verificar = false;
-					}
-					else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
-			if(!cpfValid.substring(0).matches("[0-9]*")){
-				verificar = false;
-				do {
-					System.out.println("Erro! Um CPF possui apenas numeros. Insira um CPF valido: ");
-					user.setCpf(ler.next());
-					cpfValid = user.getCpf();
-					if(!cpfValid.substring(0).matches("[0-9]*")){
-						verificar = false;
-					}
-					else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
+		return verificar;
+	}
+
+	public void cadastrarTelefone() {
+		System.out.println("------------------------------");
+		System.out.println("Cadastrar usuarios: ");
+		System.out.println("Telefone do usuario. Apenas letras maiusculas: ");
+		user.setTelefone(ler.next());
+		// validarTelefone();
+	}
+
+	public boolean validarTelefone(String telValid) {
+		System.out.println("Validando Telefone");
+		boolean verificar = true;
+		if (telValid.length() != 9) {
+			verificar = false;
 		}
-		
-		public void cadastrarSenha() {
-			System.out.println("------------------------------");
-			System.out.println("Cadastrar usuarios: ");
-			System.out.println("Senha do usuario. Apenas letras maiusculas: ");
-			user.setSenha(ler.next());
-			validarSenha();
+		if (!telValid.substring(0).matches("[0-9]*")) {
+			verificar = false;
 		}
-		public void validarSenha() {
-			System.out.println("Validando Senha");
-			boolean verificar = true;
-			String senhaValid = user.getSenha();
-			if(senhaValid.length() != 7){
-				verificar = false;
-				do {
-					System.out.println("Erro! Uma senha deve ter tamanho de 7 caracteres. \n"
-							+ "Insira uma senha valida:");
-					user.setSenha(ler.next());
-					senhaValid = user.getSenha();
-					if(senhaValid.length() != 7){
-						verificar = false;
-					}
-					else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
-			if(!senhaValid.substring(0, 3).matches("[A-Z]*")){
-				verificar = false;
-				do {
-					System.out.println("Erro! Uma senha precisa inicialmente ter 3 letras maiusculas. \n"
-							+ "Insira uma senha valida: ");
-					user.setSenha(ler.next());
-					senhaValid = user.getSenha();
-					if(!senhaValid.substring(0, 3).matches("[A-Z]*")){
-						verificar = false;
-					}
-					else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
-			if(!senhaValid.substring(3).matches("[0-9]*")){
-				verificar = false;
-				do {
-					System.out.println("Erro! Uma senha precisa ter 4 numeros em seu final. \n"
-							+ "Insira uma senha valida:");
-					user.setSenha(ler.next());
-					senhaValid = user.getSenha();
-					if(!senhaValid.substring(3).matches("[0-9]*")){
-						verificar = false;
-					}
-					else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
-		}
-		
-		public void cadastrarTelefone() {
-			System.out.println("------------------------------");
-			System.out.println("Cadastrar usuarios: ");
-			System.out.println("Telefone do usuario. Apenas letras maiusculas: ");
-			user.setTelefone(ler.next());
-			validarTelefone();
-		}
-		public void validarTelefone() {
-			System.out.println("Validando Telefone");
-			boolean verificar = true;
-			String telValid = user.getTelefone();
-			if(telValid.length() != 9){
-				verificar = false;
-				do {
-					System.out.println("Erro! Um telefone possui 9 numeros. \n"
-							+ "Insira um telefone valido: ");
-					user.setTelefone(ler.next());
-					telValid = user.getTelefone();
-					if(telValid.length() != 9){ 
-						verificar = false;
-					}
-					else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
-			if(!telValid.substring(0).matches("[0-9]*")){
-				verificar = false;
-				do {
-					System.out.println("Erro! Um telefone possui apenas numeros.\n "
-							+ "Insira um telefone valido:");
-					user.setTelefone(ler.next());
-					telValid = user.getTelefone();
-					if(!telValid.substring(0).matches("[0-9]*")){
-						verificar = false;
-					}
-					else {
-						verificar = true;
-					}
-				}while(verificar == false);
-			}
-		}
-		
-		public void editarUsuario() {
-			int opcao;
-			System.out.println("O que deseja editar??\n");
-			System.out.println("1.Nome");
-			System.out.println("2.Email");
-			System.out.println("3.RG");
-			System.out.println("4.CPF");
-			System.out.println("5.Senha");
-			System.out.println("6.Telefone");
-			opcao = ler.nextInt();
-			
-			switch(opcao) {
+		return verificar;
+	}
+
+	public void editarUsuario() {
+		int opcao;
+		System.out.println("O que deseja editar??\n");
+		System.out.println("1.Nome");
+		System.out.println("2.Email");
+		System.out.println("3.RG");
+		System.out.println("4.CPF");
+		System.out.println("5.Senha");
+		System.out.println("6.Telefone");
+		opcao = ler.nextInt();
+
+		switch (opcao) {
 			case 1:
 				cadastrarNome();
 				Usuario.usuario[ControlerLoging.indiceUser][0] = user.getNome();
@@ -355,6 +242,6 @@ public class ControlerUsuarios {
 			default:
 				System.out.println("Opcao nao encontrada");
 				break;
-			}
 		}
+	}
 }
--- TP03.git/TestTP3/src/view/TelaCadastro.java
+++ TP03.git/TestTP3/src/view/TelaCadastro.java
@@ -17,6 +17,9 @@ import java.awt.List;
 import java.awt.Canvas;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+
+import controler.ControlerUsuarios;
+
 import javax.swing.JPasswordField;
 import javax.swing.JPopupMenu;
 import java.awt.Component;
@@ -61,149 +64,161 @@ public class TelaCadastro {
 	 * Initialize the contents of the frame.
 	 */
 	public void initialize() {
+		ControlerUsuarios control = new ControlerUsuarios();
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 706);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel tituloUm = new JLabel("Insira os dados necesarios ");
 		tituloUm.setFont(new Font("Elephant", Font.PLAIN, 17));
 		tituloUm.setForeground(Color.BLACK);
 		tituloUm.setBackground(Color.CYAN);
 		tituloUm.setBounds(111, 99, 239, 31);
 		frame.getContentPane().add(tituloUm);
-		
+
 		JLabel tituloDois = new JLabel("para efetuar seu cadastro: ");
 		tituloDois.setFont(new Font("Elephant", Font.PLAIN, 17));
 		tituloDois.setBounds(121, 125, 239, 13);
 		frame.getContentPane().add(tituloDois);
-		
+
 		txtNome = new JTextField();
 		txtNome.setBounds(46, 186, 335, 20);
 		frame.getContentPane().add(txtNome);
 		txtNome.setColumns(10);
-		
+
 		JLabel tituloNome = new JLabel("Nome (Letras maiusuclas):");
 		tituloNome.setFont(new Font("Cambria", Font.PLAIN, 15));
 		tituloNome.setBounds(46, 165, 176, 14);
 		frame.getContentPane().add(tituloNome);
-		
-		JButton checkNome = new JButton("OK");
-		checkNome.setFont(new Font("Cambria", Font.PLAIN, 11));
-		checkNome.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Seu nome é: " + txtNome.getText());
-			}
-		});
-		checkNome.setBackground(new Color(51, 204, 204));
-		checkNome.setBounds(181, 212, 89, 23);
-		frame.getContentPane().add(checkNome);
-		
+
 		JLabel lblNewLabel_2_1 = new JLabel("E-mail:");
 		lblNewLabel_2_1.setFont(new Font("Cambria", Font.PLAIN, 15));
 		lblNewLabel_2_1.setBounds(46, 246, 46, 14);
 		frame.getContentPane().add(lblNewLabel_2_1);
-		
+
 		txtEmail = new JTextField();
 		txtEmail.setColumns(10);
 		txtEmail.setBounds(46, 267, 335, 20);
 		frame.getContentPane().add(txtEmail);
-		
-		JButton checkEmail = new JButton("OK");
-		checkEmail.setFont(new Font("Cambria", Font.PLAIN, 11));
-		checkEmail.setBackground(SystemColor.desktop);
-		checkEmail.setBounds(181, 293, 89, 23);
-		frame.getContentPane().add(checkEmail);
-		
+
 		JLabel tituloRG = new JLabel("RG:");
 		tituloRG.setVerticalAlignment(SwingConstants.BOTTOM);
 		tituloRG.setFont(new Font("Cambria", Font.PLAIN, 15));
 		tituloRG.setBounds(46, 327, 46, 14);
 		frame.getContentPane().add(tituloRG);
-		
+
 		txtRG = new JTextField();
 		txtRG.setColumns(10);
 		txtRG.setBounds(46, 348, 335, 20);
 		frame.getContentPane().add(txtRG);
-		
-		JButton checkRG = new JButton("OK");
-		checkRG.setFont(new Font("Cambria", Font.PLAIN, 11));
-		checkRG.setBackground(new Color(51, 204, 204));
-		checkRG.setBounds(181, 374, 89, 23);
-		frame.getContentPane().add(checkRG);
-		
+
 		JLabel tituloCPF = new JLabel("CPF:");
 		tituloCPF.setHorizontalAlignment(SwingConstants.LEFT);
 		tituloCPF.setVerticalAlignment(SwingConstants.BOTTOM);
 		tituloCPF.setFont(new Font("Cambria", Font.PLAIN, 15));
 		tituloCPF.setBounds(46, 408, 46, 14);
 		frame.getContentPane().add(tituloCPF);
-		
+
 		txtCPF = new JTextField();
 		txtCPF.setColumns(10);
 		txtCPF.setBounds(46, 429, 335, 20);
 		frame.getContentPane().add(txtCPF);
-		
-		JButton checkCPF = new JButton("OK");
-		checkCPF.setFont(new Font("Cambria", Font.PLAIN, 11));
-		checkCPF.setBackground(SystemColor.desktop);
-		checkCPF.setBounds(181, 455, 89, 23);
-		frame.getContentPane().add(checkCPF);
-		
+
 		JLabel tituloTel = new JLabel("Telefone:");
 		tituloTel.setHorizontalAlignment(SwingConstants.TRAILING);
 		tituloTel.setVerticalAlignment(SwingConstants.BOTTOM);
 		tituloTel.setFont(new Font("Cambria", Font.PLAIN, 15));
 		tituloTel.setBounds(46, 489, 59, 14);
 		frame.getContentPane().add(tituloTel);
-		
+
 		txtTel = new JTextField();
 		txtTel.setColumns(10);
 		txtTel.setBounds(46, 510, 335, 20);
 		frame.getContentPane().add(txtTel);
-		
-		JButton checkTel = new JButton("OK");
-		checkTel.setFont(new Font("Cambria", Font.PLAIN, 11));
-		checkTel.setBackground(new Color(51, 204, 204));
-		checkTel.setBounds(181, 536, 89, 23);
-		frame.getContentPane().add(checkTel);
-		
+
 		JLabel tituloSenha = new JLabel("Senha (Deve come\u00E7ar com 3 letras maiusuclas e ");
 		tituloSenha.setVerticalAlignment(SwingConstants.BOTTOM);
 		tituloSenha.setHorizontalAlignment(SwingConstants.LEFT);
 		tituloSenha.setFont(new Font("Cambria", Font.PLAIN, 15));
 		tituloSenha.setBounds(46, 570, 335, 14);
 		frame.getContentPane().add(tituloSenha);
-		
+
 		senha = new JPasswordField();
 		senha.setBounds(46, 607, 335, 20);
 		frame.getContentPane().add(senha);
-		
-		JButton checkSenha = new JButton("OK");
-		checkSenha.setFont(new Font("Cambria", Font.PLAIN, 11));
-		checkSenha.setBackground(SystemColor.desktop);
-		checkSenha.setBounds(181, 633, 89, 23);
-		frame.getContentPane().add(checkSenha);
-		
+
+		JButton check = new JButton("OK");
+		check.setFont(new Font("Cambria", Font.PLAIN, 11));
+		check.setBackground(SystemColor.desktop);
+		check.setBounds(181, 633, 89, 23);
+		check.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				String msgErro = "";
+
+				if (txtNome.getText().length() == 0 || txtEmail.getText().length() == 0 || txtRG.getText().length() == 0
+						|| txtCPF.getText().length() == 0 || txtTel.getText().length() == 0
+						|| senha.getText().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Campo Vazio mensagem erro", "Título da Janela",
+							JOptionPane.ERROR_MESSAGE);
+
+					return;
+				}
+				if (!control.validarNome(txtNome.getText())) {
+					msgErro += "Nome invalido\n";
+				}
+				if (!control.validarEmail(txtEmail.getText())) {
+					msgErro += "Email invalido\n";
+				}
+				if (!control.validarRg(txtRG.getText())) {
+					msgErro += "Rg invalido\n";
+				}
+				if (!control.validarCpf(txtCPF.getText())) {
+					msgErro += "Cpf invalido\n";
+				}
+				if (!control.validarTelefone(txtTel.getText())) {
+					msgErro += "Telefone invalido\n";
+				}
+				if (!control.validarSenha(senha.getText())) {
+					System.out.println(senha.getPassword());
+					msgErro += "Senha invalida\n";
+				}
+
+				if (msgErro.length() > 0) {
+					JOptionPane.showMessageDialog(null, msgErro, "Título da Janela", JOptionPane.ERROR_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(null, "Confirmar Cadastro", "Deseja Confirmar o Cadastro ?",
+							JOptionPane.DEFAULT_OPTION);
+
+					frame.dispose();
+					TelaMenu.main(null);
+				}
+			}
+		});
+		frame.getContentPane().add(check);
+
 		JLabel lblTerminarCom = new JLabel("terminar com 4 numeros):");
 		lblTerminarCom.setVerticalAlignment(SwingConstants.BOTTOM);
 		lblTerminarCom.setHorizontalAlignment(SwingConstants.LEFT);
 		lblTerminarCom.setFont(new Font("Cambria", Font.PLAIN, 15));
 		lblTerminarCom.setBounds(46, 584, 335, 14);
 		frame.getContentPane().add(lblTerminarCom);
-		
+
 		JLabel lblCadastro = new JLabel("Cadastro");
 		lblCadastro.setForeground(SystemColor.desktop);
 		lblCadastro.setFont(new Font("Elephant", Font.PLAIN, 37));
 		lblCadastro.setBounds(146, 25, 204, 75);
 		frame.getContentPane().add(lblCadastro);
-		
+
 		JLabel tiruloUm_1 = new JLabel("Cadastro");
 		tiruloUm_1.setForeground(new Color(51, 204, 204));
 		tiruloUm_1.setFont(new Font("Elephant", Font.PLAIN, 37));
 		tiruloUm_1.setBounds(136, 11, 194, 75);
 		frame.getContentPane().add(tiruloUm_1);
 	}
+
 	private static void addPopup(Component component, final JPopupMenu popup) {
 		component.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
@@ -211,11 +226,13 @@ public class TelaCadastro {
 					showMenu(e);
 				}
 			}
+
 			public void mouseReleased(MouseEvent e) {
 				if (e.isPopupTrigger()) {
 					showMenu(e);
 				}
 			}
+
 			private void showMenu(MouseEvent e) {
 				popup.show(e.getComponent(), e.getX(), e.getY());
 			}
